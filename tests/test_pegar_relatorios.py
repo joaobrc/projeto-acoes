467f21bf091759246b56c510cd4aac580dfb5e5f
Joao <joaobrc97@gmail.com>
2025-09-01 21:42:38 -0300

    :test_tube: test: Teste da Rotina de adquirir relatorios

---
+++ tests/test_pegar_relatorios.py
@@ -0,0 +1,12 @@
+from src.projeto_acoes.pegar_relatorios import RelatorioFii
+
+
+def test_pegar_relatorios():
+    relatorios = RelatorioFii(
+        sigla_fundo = 'GARE11',
+        data_inicial = '2023-01-01',
+        data_final = '2023-12-31'
+    )
+    
+
+    assert relatorios is not None
