3864bf38e2e587f5ef6aee3412780ad0dbd0581a
Joao <joaobrc97@gmail.com>
2025-09-04 21:42:14 -0300

    :sparkles: feat: Add dados_pagina_fundos method for fetching fund data

--- src/projeto_acoes/pegar_relatorios.py
+++ src/projeto_acoes/pegar_relatorios.py
@@ -1,4 +1,5 @@
 from httpx import Client
+from bs4 import BeautifulSoup
 
 
 class RelatorioFii:
@@ -6,6 +7,16 @@ class RelatorioFii:
         self.sigla_fundo = sigla_fundo
         self.data_inicial = data_inicial
         self.data_final = data_final
+        self.cliente = Client()
 
     def __repr__(self):
         return f'RelatorioFii(sigla_fundo={self.sigla_fundo}, data_inicial={self.data_inicial}, data_final={self.data_final})'
+
+    def dados_pagina_fundos(self):
+        fundos = {'GARE11': '37295919000160'}
+        url = f'https://fnet.bmfbovespa.com.br/fnet/publico/abrirGerenciadorDocumentosCVM?cnpjFundo={fundos.get(self.sigla_fundo)}'
+        response = self.cliente.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Aqui você pode adicionar a lógica para extrair os dados desejados da página
+        return soup.prettify()
