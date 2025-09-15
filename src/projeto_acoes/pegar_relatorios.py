aacab203a4eaded3db8afd0c64e4fab1f7c328ae
Joao <joaobrc97@gmail.com>
2025-09-14 21:25:07 -0300

    refactor: Update CadastroFundos and RelatorioFii classes for improved database handling and consistency

--- src/projeto_acoes/pegar_relatorios.py
+++ src/projeto_acoes/pegar_relatorios.py
@@ -1,13 +1,12 @@
 from httpx import Client
-from src.projeto_acoes.database import SessionLocal, create_tables
-from src.projeto_acoes.model import Documento, FundosImobiliarios
+from src.projeto_acoes.model import FundosII, Acoes, DocumentosFII
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 
 class CadastroFundos:
-    def __init__(self):
-        self.db = SessionLocal()
-        create_tables()
-        pass
+    def __init__(self, db: Session = None):
+        self.db: Session = db
 
     def cadastrar_fundos(
         self,
@@ -15,32 +14,27 @@ class CadastroFundos:
         sigla_fundo: str,
         cnpj_fundo: str,
     ):
-        fundo = FundosImobiliarios(
-            nome=nome_fundo,
-            sigla=sigla_fundo,
-            cnpj=cnpj_fundo
-        )
+        fundo = FundosII(nome=nome_fundo, sigla=sigla_fundo, cnpj=cnpj_fundo)
         self.db.add(fundo)
         self.db.commit()
         self.db.refresh(fundo)
         return fundo
 
     def get_fundos(self):
-        return self.db.query(FundosImobiliarios).all()
+        return self.db.scalars(select(FundosII)).all()
 
     def get_fundo_por_sigla(self, sigla: str):
-        fundo = (
-            self.db.query(FundosImobiliarios)
-            .filter(FundosImobiliarios.sigla == sigla)
-            .first()
-        )
+        fundo = self.db.scalar(select(FundosII).where(FundosII.sigla == sigla))
         if not fundo:
             raise ValueError(f'Fundo com sigla {sigla} não encontrado.')
         return fundo
 
 
 class RelatorioFii:
-    def __init__(self, sigla_fundo: str, data_inicial: str, data_final: str):
+    def __init__(
+        self, db: Session, sigla_fundo: str, data_inicial: str, data_final: str
+    ):
+        self.db = db
         self.sigla_fundo = sigla_fundo
         self.data_inicial = data_inicial
         self.data_final = data_final
@@ -54,10 +48,9 @@ class RelatorioFii:
             'idEspecieDocumento': '0',
             'isSession': 'true',
         }
-        self.db = SessionLocal()
         self.cliente = Client()
 
-    def _tratar_resposta(self, response):
+    def _tratar_resposta(self, fundo_id: int, fundo_sigla: str, response):
         if response.status_code != 200:
             raise Exception(f'Erro na requisição: {response.status_code}')
         else:
@@ -65,20 +58,18 @@ class RelatorioFii:
             dados = response.json()
 
             try:
-                # Get or create the fund
-                fundo = self._get_or_create_fundo(self)
-
                 for item in dados.get('data', []):
                     doc_data = {
                         'id_documento': str(item.get('id')),
                         'descricao': item.get('tipoDocumento'),
-                        'fundo': item.get('descricaoFundo'),
+                        'titulo': item.get('descricaoFundo'),
+                        'data_entrega': item.get('dataEntrega'),
                     }
+                    doc_data['id_sigla_fundo'] = fundo_id
                     dados_documentos.append(doc_data)
-
                     # Save to DB with relationship
-                    documento = Documento(**doc_data)
-                    documento.fundo_relacionado = fundo
+                    documento = DocumentosFII(**doc_data)
+
                     self.db.merge(documento)  # Use merge to avoid duplicates
                 self.db.commit()
             except Exception as e:
@@ -90,11 +81,12 @@ class RelatorioFii:
             return dados_documentos
 
     def dados_pagina_fundos(self):
-        fundos = CadastroFundos().get_fundo_por_sigla(self.sigla_fundo)
+        fundos = CadastroFundos(self.db)
+        fundo = fundos.get_fundo_por_sigla(sigla=self.sigla_fundo)
         self.params.update(
             {
-                'cnpj': fundos.cnpj,
-                'cnpjFundo': fundos.cnpj,
+                'cnpj': fundo.cnpj,
+                'cnpjFundo': fundo.cnpj,
                 'dataInicio': self.data_inicial,
                 'dataFim': self.data_final,
             }
@@ -104,4 +96,6 @@ class RelatorioFii:
         response = self.cliente.get(url, params=self.params)
         response.raise_for_status()
 
-        return self._tratar_resposta(response)
+        return self._tratar_resposta(
+            fundo_id=fundo.id, fundo_sigla=fundo.sigla, response=response
+        )
