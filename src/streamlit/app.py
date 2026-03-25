a7a257cf960428d7c6825f9da40e7844a7d358d4
Joao <joaobrc97@gmail.com>
2026-03-24 22:15:25 -0300

    feat: Implement fund registration feature in Streamlit app

--- src/streamlit/app.py
+++ src/streamlit/app.py
@@ -1,15 +1,29 @@
 import streamlit as st
-from projeto_acoes.pegar_relatorios import RelatorioFii
+from projeto_acoes.pegar_relatorios import RelatorioFii, CadastroFundos
+from projeto_acoes.database import get_db
 
 st.title("Doc Ações")
 
-with st.sidebar:
-    st.sidebar.header("Menu Lateral")
-    st.sidebar.write("Formulário de Ações")
-    acao = st.sidebar.text_input("Digite o nome da ação")
-    data_pesquisa = st.sidebar.date_input("Data de Pesquisa Inicial", format="DD/MM/YYYY")
-    data_pesquisa_final = st.sidebar.date_input("Data de Pesquisa Final", format="DD/MM/YYYY")
-    pesquisa = st.sidebar.button("Pesquisar")
+aba_cadastra, aba_relatorios = st.tabs(["Cadastro de Fundos", "Relatórios de Fundos"])
 
-if pesquisa:
-    pesquisa_fii = RelatorioFii()
+@st.cache_data
+def get_fundos():
+    db = next(get_db())
+    cadastro = CadastroFundos(db)
+    return [{'Nome': fundo.nome, 'Sigla': fundo.sigla, 'CNPJ': fundo.cnpj} for fundo in cadastro.get_fundos()]
+
+
+with aba_cadastra:
+    dados_fundos = get_fundos()
+    st.write(dados_fundos)
+    st.header("Cadastro de Fundos")
+    nome_fundo = st.text_input("Nome do Fundo")
+    sigla_fundo = st.text_input("Sigla do Fundo")
+    cnpj_fundo = st.text_input("CNPJ do Fundo")
+    if st.button("Cadastrar Fundo"):
+        with st.spinner("Cadastrando fundo..."):
+            cadastro = CadastroFundos(db=next(get_db()))
+            fundo = cadastro.cadastrar_fundos(nome_fundo, sigla_fundo, cnpj_fundo)
+            dados_fundos = get_fundos()
+            st.write(dados_fundos)
+            st.success(f"Fundo {fundo.nome} cadastrado com sucesso!")
